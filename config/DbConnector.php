415bbeed818491f8b04fb74f01f12ed1a5b3135a
Lyuboslav Mitov <lmitov@uni-sofia.bg>
2019-07-01 19:41:51 +0300

    Updated the db connector

--- config/DbConnector.php
+++ config/DbConnector.php
@@ -1,26 +1,38 @@
 <?php
 class DbConnector
 {
-    private $host = "localhost";
-    private $db_name = "daily_scrum";
-    private $username = "root";
-    private $password = "";
-    public $conn;
+    private static $host = "localhost";
+    private static $db_name = "daily_scrum";
+    private static $username = "root";
+    private static $password = "";
+    private static $conn=null;
 
-    public function getConnection()
-    {
-        $this->conn = null;
-        try {
-            $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
-                $this->username,
-                $this->password
-            );
-            $this->conn->exec("set names utf8");
-        } catch (PDOException $exception) {
-            echo "Connection error: " . $exception->getMessage();
+    // public function getConnection()
+    // {
+    //     $this->conn = null;
+    //     try {
+    //         $this->conn = new PDO(
+    //             "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+    //             $this->username,
+    //             $this->password
+    //         );
+    //         $this->conn->exec("set names utf8");
+    //     } catch (PDOException $exception) {
+    //         echo "Connection error: " . $exception->getMessage();
+    //     }
+
+    //     return $this->conn;
+    // }
+    public static function getConnection() {
+        if(is_null(self::$conn)) {
+            try
+            {
+                self::$conn = new PDO("mysql:host=" . $host . ";dbname=" . $db_name,$username,$password);
+            }
+            catch (PDOException $exception) {
+                echo "Connection error: " . $exception->getMessage();
+            }
         }
-
-        return $this->conn;
+        return self::$conn;
     }
 }
